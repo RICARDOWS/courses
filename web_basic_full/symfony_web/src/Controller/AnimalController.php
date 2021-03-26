9b2ba6f9b55d11c5d36ab5ae2335f3454354c79c
Ricardo Bebeto Jimenez Garcia <bbto_j_g@hotmail.com>
2021-03-26 15:57:31 -0700

    new symfony project

---
+++ web_basic_full/symfony_web/src/Controller/AnimalController.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Animal;
+
+class AnimalController extends AbstractController
+{
+    /**
+     * @Route("/animal", name="animal")
+     */
+    public function index(): Response
+    {
+        $entityRepo = $this->getDoctrine()->getRepository(Animal::class);
+
+        $animales = $entityRepo->findAll();
+
+        return $this->render('animal/index.html.twig', [
+            'controller_name' => 'AnimalController',
+            'animales' => $animales
+        ]);
+    }
+
+    public function store(): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $animal = new Animal();
+        $animal->setNombre('Tortuga');
+        $animal->setEspecie('Reptil');
+        $animal->setEdad('20');
+
+        $entityManager->persist($animal);
+        $entityManager->flush();
+
+        return new Response('El animal guardado tiene el id: '.$animal->getId());
+    }
+
+    public function detail(Animal $animal):Response
+    {
+//        $entityRepo = $this->getDoctrine()->getRepository(Animal::class);
+//
+//        $animal = $entityRepo->find($id);
+
+        if($animal === null){
+            return new Response('No se encontro animal');
+        }
+
+        return new Response('El animal es: '.$animal->getNombre().' de edad: '.$animal->getEdad());
+    }
+}
---
+++ web_basic_full/symfony_web/src/Entity/Animal.php
@@ -0,0 +1,75 @@
+<?php
+namespace App\Entity;
+
+use App\Repository\AnimalRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=AnimalRepository::class)
+ */
+class Animal
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $especie;
+
+    /**
+     * @ORM\Column(type="integer", length=255)
+     */
+    private $edad;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getEspecie(): ?string
+    {
+        return $this->especie;
+    }
+
+    public function setEspecie(string $especie): self
+    {
+        $this->especie = $especie;
+
+        return $this;
+    }
+
+    public function getEdad(): ?int
+    {
+        return $this->edad;
+    }
+
+    public function setEdad(int $edad): self
+    {
+        $this->edad = $edad;
+
+        return $this;
+    }
+
+}
