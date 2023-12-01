d21d76fdf92ee852b7efcf3e770f3fa45b6c57b6
RICARDOWS <44734657+RICARDOWS@users.noreply.github.com>
2023-12-01 13:20:02 -0700

    vue

---
+++ vuejs/07-validacion-formulario/src/App.vue
@@ -0,0 +1,82 @@
+<template>
+ <form @submit.prevent="enviar" class="form">
+  <my-input 
+    name="Usuario" 
+    :rules="{ required: true, min:5 }"
+    :value="usuario.value"
+    @update="update"
+    type="text"
+  />
+  
+  <my-input 
+    name="Password" 
+    :rules="{ required: true, min:10 }"
+    :value="password.value"
+    @update="update"
+    type="password"
+  />
+   
+  <my-button 
+    color="white" 
+    background="darkslateblue" 
+    :disabled="!valid" 
+  /> 
+ </form> 
+
+</template>
+
+<script>
+import MyButton from './components/MyButton.vue'
+import MyInput from './components/MyInput.vue'
+
+export default {
+  name: 'App',
+  components: {
+    MyButton,
+    MyInput
+  },
+  data(){
+    return {      
+      usuario: {
+        value: '',
+        valid: false
+      },
+      password:{
+        value: '',
+        valid: false
+      } 
+    }
+  },
+  computed: {
+    valid(){
+      return this.usuario.valid && this.password.valid
+    }
+  },
+  methods:{
+    enviar(){      
+      console.log('Enviar');
+
+      //Código para validar el formulario
+    },
+    update(payload){      
+      this[payload.name.toLowerCase()] = {
+        value: payload.value,
+        valid: payload.valid
+      };
+    }
+  }
+}
+</script>
+
+<style>
+
+ body{
+   font-family: arial;
+ }
+
+ .form{
+   max-width: 400px;
+   width: 50%;
+ }
+
+</style>
